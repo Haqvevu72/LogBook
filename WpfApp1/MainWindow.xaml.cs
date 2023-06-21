baa657954f95e6235a8eb0ed0c7100676b382810
Haqve_vu72 <Haqve_vu72@itstep.edu.az>
2023-06-21 09:20:38 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Student> students = new List<Student>() 
+        {
+            new Student("Pictures\\R.png","Haqverdiyev Elgun Elnur",0,"Very Funny Guy",3),
+            new Student("Pictures\\R.png","Nagiyev Gunduz Veli",0,"Professional",3),
+            new Student("Pictures\\R.png","Eminli Hikmet Yalchin",0,"Blessed",3)
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            LogBook.ItemsSource = students;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button comment = sender as Button;
+            StackPanel logbook=comment.Parent as StackPanel;
+            TextBox message = logbook.Children[7] as TextBox;
+            if (message.Visibility == Visibility.Visible) { message.Visibility = Visibility.Hidden; }
+            else { message.Visibility = Visibility.Visible; }
+        }
+    }
+}
---
+++ WpfApp1/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Student
+    {
+        public Student(string avatar, string nasurfath, int average, string comment, int attendance)
+        {
+            id = ++ID;
+            this.avatar = avatar;
+            this.nasurfath = nasurfath;
+            this.average = average;
+            this.comment = comment;
+            this.attendance = attendance;
+        }
+
+        public static int ID { get; set; } = 0;
+        public int id { get; set; }
+        public string avatar { get; set; }
+        public string nasurfath { get; set; }
+        public int average { get; set; }
+        public string comment { get; set; }
+        public int attendance { get; set; }
+
+        
+    }
+}
